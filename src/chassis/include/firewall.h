e49e249184bee6e7f250c12ea132761bafb18f1d
Sam Sherman <shermanhlc@proton.me>
2025-05-30 17:22:23 -0500

    structure/cmake for chassis components

---
+++ src/chassis/include/firewall.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// #include <vector>
+#include <utils/point.h>
+
+namespace chassis
+{
+    class firewall
+    {
+        public:
+        
+        private:
+            double tolerance;
+            std::unordered_map<QString, point> pointMap;
+    }
+}
