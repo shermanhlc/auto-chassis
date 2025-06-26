268fa64294da2a0e0cbac6aba7f6ff7d177d689b
Sam Sherman <shermanhlc@proton.me>
2025-06-25 19:24:55 -0500

    docker work

---
+++ packer/default.nix
@@ -0,0 +1,26 @@
+let 
+    nixpkgsSrc = builtins.fetchTarball {
+        name = "nixos-25.05-small";
+        url = "https://github.com/NixOS/nixpkgs/archive/a5e9291e97f5ba0b4ba7d657ddedd5f86d11acfd.tar.gz";
+        sha256 = "05q8vi2pgl91ya2nav2hi8zg4xy2vw90s6n3a567jfxbly885011";
+    };
+    pkgs = import nixpkgsSrc {};
+
+in 
+pkgs.mkShell {
+    buildInputs = [
+        pkgs.git
+        pkgs.cacert
+
+        pkgs.cmake
+        pkgs.gcc15
+
+        pkgs.qt6.qtbase
+        pkgs.qt6.wrapQtAppsHook
+        pkgs.tomlplusplus
+    ];
+
+    shellHook = ''
+        echo "env ready"
+    '';
+}
